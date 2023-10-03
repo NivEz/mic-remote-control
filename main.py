ebe5a2a061cb8008d90f47734b404f7060a9db2a
Niv Ezra <niv.cyber@gmail.com>
2023-10-03 13:03:32 +0300

    printing ipv4 address before server startup

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 import uvicorn
+import socket
 from mic import Mic
 
 app = FastAPI()
@@ -16,7 +17,6 @@ def mute_or_unmute_mic():
 
 app.mount("/", StaticFiles(directory="static",html = True), name="static")
 
-
 def get_user_input():
     is_muted = input("Is the mic currently muted? (y/n)\n")
     if is_muted.lower() == "y":
@@ -27,7 +27,13 @@ def get_user_input():
         print('Invalid input (only "y" and "n" are valid). Please try again.')
         return get_user_input()
 
+def get_ip_address():
+    hostname = socket.gethostname()
+    ip_address = socket.gethostbyname(hostname)
+    return ip_address
+
 if __name__ == '__main__':
     IS_MUTED = get_user_input()
     mic = Mic(IS_MUTED)
+    print(f"\nTher server will be hosted on IPv4 address: [{get_ip_address()}]\n")
     uvicorn.run(app, host='0.0.0.0', port=8000)
